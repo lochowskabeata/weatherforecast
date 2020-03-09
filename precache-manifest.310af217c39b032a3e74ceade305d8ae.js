d11b4be03361123477c11e0e961a2f528fab9aaf
Beata Lochowska <kali.lubi.krowy@gmail.com>
2020-03-09 09:08:31 +0100

    Updates

---
+++ precache-manifest.310af217c39b032a3e74ceade305d8ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fedb415c7ea44641b95fc219f2079a0f",
+    "url": "/weatherforecast/index.html"
+  },
+  {
+    "revision": "3b3da942a086727d88d6",
+    "url": "/weatherforecast/static/css/main.e49df320.chunk.css"
+  },
+  {
+    "revision": "13fb73b6afe1303a8487",
+    "url": "/weatherforecast/static/js/2.3a48ee07.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/weatherforecast/static/js/2.3a48ee07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b3da942a086727d88d6",
+    "url": "/weatherforecast/static/js/main.df33184c.chunk.js"
+  },
+  {
+    "revision": "c2f68b39a99b632286f0",
+    "url": "/weatherforecast/static/js/runtime-main.afb71f42.js"
+  }
+]);
